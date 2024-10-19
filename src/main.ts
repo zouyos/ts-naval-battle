c089eb5a4e5d110bb19393ddde84c4f33f29769e
benket_h <hamza.benketaf@gmail.com>
2024-10-19 09:40:06 +0000

    v1 update

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ const rl = require('readline-sync');
 // const answer = rl.keyInYN('test?')
 // const answer2 = rl.keyInSelect(['1', '2', '3'], 'test?')
 
+const number = 3
+
 const letters: string[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 
 function fillChoices(arr: string[]) {
@@ -28,7 +30,19 @@ function displayBoard(mat: string[][]) {
   });
 }
 
-function placeBoat(square: string, mat: string[][]) {
+function displayFakeBoard(mat: string[][]) {
+  const newMat = mat.map(row => [...row]);
+  newMat.forEach((arr) => {
+    arr.forEach((square, i) => {
+      if (square === 'O') {
+        arr[i] = '.'
+      }
+    })
+  });
+  displayBoard(newMat)
+}
+
+function placeShip(square: string, mat: string[][]) {
   const row: number = Number(square.charAt(1));
   const col: number = letters.indexOf(square.charAt(0));
   mat[row - 1][col] = 'O'
@@ -41,59 +55,90 @@ function togglePlayer1() {
 function targetSquare(square: string, mat: string[][]) {
   const row: number = Number(square.charAt(1));
   const col: number = letters.indexOf(square.charAt(0));
-  if (mat[row - 1][col] !== '.') {
+  if (mat[row - 1][col] === 'O') {
     console.log('HIT!');
     mat[row - 1][col] = 'X'
+  } else if (mat[row - 1][col] === 'X') {
+    console.log('You\'ve already targeted this square');
   } else {
     console.log('Missed...');
   }
 }
 
 function winCheck(mat: string[][]) {
-  return mat.some((arr) => arr.some((square: string) => square === 'X'))
+  return !mat.some((arr) => arr.some((square: string) => square === 'O'))
 }
 
 function play() {
-
   console.log('Welcome to this naval battle game');
   displayBoard(matrix1);
 
-  let firstBoatP1 = rl.question('Player 1 place a boat: ')
-  firstBoatP1 = firstBoatP1.toLocaleUpperCase()
-  while (!choices.includes(firstBoatP1)) {
-    console.log('Please provide a valid value.');
-    firstBoatP1 = rl.question('Player 1 place a boat: ')
-  }
-  placeBoat(firstBoatP1, matrix1)
-  displayBoard(matrix1);
+  let player1Ships: string[] = []
+  let player2Ships: string[] = []
 
-  let firstBoatP2 = rl.question('Player 2 place a boat: ')
-  firstBoatP2 = firstBoatP2.toLocaleUpperCase()
-  while (!choices.includes(firstBoatP2)) {
-    console.log('Please provide a valid value.');
-    firstBoatP2 = rl.question('Player 1 place a boat: ')
+  while (player1Ships.length < 3) {
+    let firstShipP1 = rl.question('Player 1 place a ship: ').toLocaleUpperCase()
+    while (!choices.includes(firstShipP1)) {
+      console.log('Please provide an available square.');
+      displayBoard(matrix1);
+      firstShipP1 = rl.question('Player 1 place a ship: ').toLocaleUpperCase()
+    }
+    let rowFirstShipP1: number = Number(firstShipP1.charAt(1));
+    let colFirstShipP1: number = letters.indexOf(firstShipP1.charAt(0));
+    while (matrix1[rowFirstShipP1 - 1][colFirstShipP1] === 'O') {
+      console.log('There is already a ship on this square.');
+      firstShipP1 = rl.question('Player 1 place a ship: ').toLocaleUpperCase()
+      rowFirstShipP1 = Number(firstShipP1.charAt(1));
+      colFirstShipP1 = letters.indexOf(firstShipP1.charAt(0));
+    }
+    placeShip(firstShipP1, matrix1)
+    player1Ships.push(firstShipP1)
+    console.log('Ship placed.');
+    displayBoard(matrix1);
   }
-  placeBoat(firstBoatP2, matrix2)
-  displayBoard(matrix2);
+
+  console.log('All ships placed for player 1');
+
+  while (player2Ships.length < 3) {
+    let firstShipP2 = rl.question('Player 2 place a ship: ').toLocaleUpperCase()
+    while (!choices.includes(firstShipP2)) {
+      console.log('Please provide an available square.');
+      displayBoard(matrix2);
+      firstShipP2 = rl.question('Player 2 place a ship: ').toLocaleUpperCase()
+    }
+    let rowFirstShipP2: number = Number(firstShipP2.charAt(1));
+    let colFirstShipP2: number = letters.indexOf(firstShipP2.charAt(0));
+    while (matrix2[rowFirstShipP2 - 1][colFirstShipP2] === 'O') {
+      console.log('There is already a ship on this square.');
+      firstShipP2 = rl.question('Player 2 place a ship: ').toLocaleUpperCase()
+      rowFirstShipP2 = Number(firstShipP2.charAt(1));
+      colFirstShipP2 = letters.indexOf(firstShipP2.charAt(0));
+    }
+    placeShip(firstShipP2, matrix2)
+    player2Ships.push(firstShipP2)
+    console.log('Ship placed.');
+    displayBoard(matrix2);
+  }
+
+  console.log('All ships placed for player 2');
 
   console.log('Now let\'s play!');
   while (!(winCheck(matrix1) || winCheck(matrix2))) {
-    let playerMove = rl.question(`${player1Play ? 'Player 1' : 'Player 2'} target a square: `)
-    playerMove = playerMove.toLocaleUpperCase()
+    let playerMove = rl.question(`${player1Play ? 'Player 1' : 'Player 2'} target a square: `).toLocaleUpperCase()
     if (!choices.includes(playerMove)) {
       console.log('Please provide a valid value.');
     } else {
       if (player1Play) {
         targetSquare(playerMove, matrix2)
         console.log('Opponent\'s board:');
-        displayBoard(matrix2)
+        displayFakeBoard(matrix2)
         console.log('Your board:');
         displayBoard(matrix1)
         togglePlayer1()
       } else {
         targetSquare(playerMove, matrix1)
         console.log('Opponent\'s board:');
-        displayBoard(matrix1)
+        displayFakeBoard(matrix1)
         console.log('Your board:');
         displayBoard(matrix2)
         togglePlayer1()
